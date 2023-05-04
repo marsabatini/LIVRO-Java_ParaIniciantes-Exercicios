7f293b6b969d8c1f078398d8a01bcf80a4333229
marsabatini <marco_rsabatini@hotmail.com>
2023-05-04 18:23:02 -0300

    V. 0.03

--- Cap_08/Exemplo0011/MyIF.java
+++ Cap_08/Exemplo0011/MyIF.java
@@ -1,2 +1,23 @@
-package Cap_08.Exemplo0011;public interface MyIF {
+package Cap_08.Exemplo0011;
+
+    // Esse é apenas um exemplo de como um método static
+    // pode ser chamado a partir de uma interface
+
+public interface MyIF {
+
+    // Esta é a declaracão "comum" de método de uma interface.
+    // Ela NÃO define uma implementacão padrão.
+
+    int getUserID();
+
+    // Este é um método padrão.
+    // Observe que ele fornece uma implementacão padrão.
+    default int getAdminID(){
+        return 1;
+    }
+
+    // Este é o método estático da interface
+    static int getUniversalID(){
+        return 0;
+    }
 }
---
+++ Cap_09/Exemplo0001/ExcDemo1.java
@@ -0,0 +1,26 @@
+package Cap_09.Exemplo0001;
+
+    // O programa a seguir gera propositalmente uma excecão em um array.
+    // Em seguida, trata essa excecão.
+
+public class ExcDemo1 {
+
+    public static void main(String args[]){
+
+        int nums[] = new int[4];
+
+        // Cria um bloco try
+        try{
+            System.out.println("Before exception is generated.");
+
+            //Gera uma excećão de índice fora dos limites.
+            nums[7] = 10;       // Tenta indexar excedento o limite do array nums
+            System.out.println("This won't be displayed.");
+        }
+        // Captura a excecão
+        catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement.");
+    }
+}
