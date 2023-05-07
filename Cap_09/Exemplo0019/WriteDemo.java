3e6318b348ea089031538af0a45ae95a0a12d5e4
marsabatini <marco_rsabatini@hotmail.com>
2023-05-07 07:38:59 -0300

    V. 0.06

---
+++ Cap_09/Exemplo0019/WriteDemo.java
@@ -0,0 +1,13 @@
+package Cap_09.Exemplo0019;
+
+    // Exemplo simples usando write() no lugar de print()
+
+public class WriteDemo {
+    public static void main(String args[]){
+        int b;
+        b = 'X';
+
+        System.out.write(b);            // Exibe um byte na tela
+        System.out.write('\n');
+    }
+}
