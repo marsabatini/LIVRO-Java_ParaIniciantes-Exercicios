3f19390a69ca2a6a3c0349a63809d7e06bb95ddf
marsabatini <marco_rsabatini@hotmail.com>
2023-05-07 07:45:39 -0300

    V. 0.06

---
+++ Cap_09/Exemplo0020/FormatDemo.java
@@ -0,0 +1,20 @@
+package Cap_09.Exemplo0020;
+
+    // Exemplo do use de format()
+
+import java.util.Scanner;
+
+public class FormatDemo {
+
+    public static void main(String args[]){
+
+        Scanner entrada = new Scanner(System.in);
+
+        String nome = "Marco Sabatini";
+
+        System.out.format("Olá, meu nome é %s e o seu?\n", nome);
+        System.out.print("Digite seu nome: ");
+        String visitante = entrada.nextLine();
+        System.out.format("Prazer em conhecê-lx %s", visitante);
+    }
+}
