6ab0dd694161cecbda6eb6ba889b62c180f28283
marsabatini <marco_rsabatini@hotmail.com>
2023-05-10 15:04:40 -0300

    V. 0.09

--- Cap_10/Exemplo0003/ExtendThread.java
+++ Cap_10/Exemplo0003/ExtendThread.java
@@ -1,2 +1,24 @@
-package Cap_10.Exemplo0003;public class ExtendThread {
+package Cap_10.Exemplo0003;
+
+import Cap_10.Exemplo0002.MyThread2;
+
+public class ExtendThread{
+
+    public static void main(String args[]){
+
+        System.out.println("Main thread starting.");
+
+        MyThread3 mt = new MyThread3("Child #1");
+
+        for(int i = 0; i < 50; i++){
+            System.out.print(".");
+            try{
+                Thread.sleep(100);
+            } catch(InterruptedException exception){
+                System.out.println("Main thread interrupted.");
+            }
+        }
+
+        System.out.println("Main thread ending.");
+    }
 }
