9819995f5a6904f77191743ede3ad896e55ec57a
marsabatini <marco_rsabatini@hotmail.com>
2023-05-11 14:31:54 -0300

    V. 1.00

---
+++ Cap_10/Exemplo0005/MoreThreads2.java
@@ -0,0 +1,23 @@
+package Cap_10.Exemplo0005;
+
+public class MoreThreads2 {
+
+    public static void main(String args[]){
+
+        System.out.println("Main thread starting.");
+
+        MyThread5 mt1 = new MyThread5("Child #1");
+        MyThread5 mt2 = new MyThread5("Child #2");
+        MyThread5 mt3 = new MyThread5("Child #3");
+
+        do{
+            System.out.print(".");
+            try{
+                Thread.sleep(100);
+            } catch(InterruptedException exception){
+                System.out.println("Main thread interrupted");
+            }
+        }
+
+    }
+}
---
+++ Cap_10/Exemplo0005/MyThread5.java
@@ -0,0 +1,25 @@
+package Cap_10.Exemplo0005;
+
+public class MyThread5 implements Runnable{
+
+    Thread thrd;
+
+    MyThread5(String name){
+        thrd = new Thread(this, name);
+        thrd.start();
+    }
+
+    public void run(){
+        System.out.println(thrd.getName() + " starting.");
+
+        try{
+            for(int count =0; count < 10; count++){
+                Thread.sleep(400);
+                System.out.println("In " + thrd.getName() + ", count is " + count);
+            }
+        } catch(InterruptedException exception){
+            System.out.println(thrd.getName() + " interrupted.");
+        }
+        System.out.println(thrd.getName() + " terminating.");
+    }
+}
