06046a6d6bd1fec0ed7375a41072fb3b70551138
marsabatini <marco_rsabatini@hotmail.com>
2023-05-13 12:17:01 -0300

    V. 1.02

---
+++ Cap_11/Exemplo0002/Transport.java
@@ -0,0 +1,9 @@
+package Cap_11.Exemplo0002;
+
+    // Usa os métodos de enumeracão internos
+
+    // Enumeracão de meios de transporte
+
+public enum Transport {
+    CAR, TRUCK, AIRPLANE, TRAIN, BOAT
+}
