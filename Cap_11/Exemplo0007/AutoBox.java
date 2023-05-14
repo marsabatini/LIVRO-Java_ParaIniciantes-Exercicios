99c5e03dd87278e5874aac9fe64d50e1e86ea170
marsabatini <marco_rsabatini@hotmail.com>
2023-05-14 08:26:49 -0300

    V. 1.03

---
+++ Cap_11/Exemplo0007/AutoBox.java
@@ -0,0 +1,12 @@
+package Cap_11.Exemplo0007;
+
+public class AutoBox {
+    public static void main(String[] args) {
+
+        Integer iOb = 100;      // faz o autobox de um int
+
+        int i = iOb;            // autounbox
+
+        System.out.println(i + " " + iOb);
+    }
+}
