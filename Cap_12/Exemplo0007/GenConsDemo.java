ba90bc1384323e625e29b716cdd21474894f9df5
marsabatini <marco_rsabatini@hotmail.com>
2023-05-16 14:09:57 -0300

    V. 1.05

---
+++ Cap_12/Exemplo0007/GenConsDemo.java
@@ -0,0 +1,10 @@
+package Cap_12.Exemplo0007;
+
+public class GenConsDemo {
+
+    public static void main(String[] args) {
+        Summation ob = new Summation(4.0);
+
+        System.out.println("Summation of 4.0 is " + ob.getSum());
+    }
+}
