fcaf4e5970430932e58fe1a241ba5ca22b87dab1
marsabatini <marco_rsabatini@hotmail.com>
2023-05-16 14:13:29 -0300

    V. 1.05

---
+++ Cap_12/Containment.java
@@ -0,0 +1,17 @@
+package Cap_12;
+
+    // Exemplo de interface genérica
+
+    /*
+        Uma interface genérico que lida com armazenamento.
+        Ela requer que a classe usuária tenha um ou mais
+        valores.
+     */
+
+public interface Containment<T> {
+
+    // O método contains() verifica se um item
+    // especificado está contido em um objeto
+    // que implementa Containment.
+    boolean contains(T o);
+}
