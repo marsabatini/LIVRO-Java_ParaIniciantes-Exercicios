35ba7c10bf4303366e99da74bcb996661d3b9e57
marsabatini <marco_rsabatini@hotmail.com>
2023-05-16 14:16:47 -0300

    V. 1.05

---
+++ Cap_12/Exemplo0007/MyClass.java
@@ -0,0 +1,24 @@
+package Cap_12.Exemplo0007;
+
+    // Implementa Containment usando um array.
+
+import Cap_12.Containment;
+
+// Toda classe que implementa uma interface
+// genérica também deve ser genérica.
+public class MyClass <T> implements Containment<T> {
+
+    T[] arrayRef;
+
+    MyClass(T[] o){
+        arrayRef = o;
+    }
+
+    // Implementa contains()
+    public boolean contains(T o){
+        for(T x : arrayRef)
+            if(x.equals(o))
+                return true;
+        return false;
+    }
+}
