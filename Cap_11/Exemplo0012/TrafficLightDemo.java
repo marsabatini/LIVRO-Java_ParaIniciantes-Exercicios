3a66a19597c0efbb903c9c96a88021de0d6d87f6
marsabatini <marco_rsabatini@hotmail.com>
2023-05-17 16:38:13 -0300

    V. 1.03

---
+++ Cap_11/Exemplo0012/TrafficLightDemo.java
@@ -0,0 +1,16 @@
+package Cap_11.Exemplo0012;
+
+
+public class TrafficLightDemo {
+    public static void main(String args[]){
+
+        TrafficLightSimulator t1 =  new TrafficLightSimulator(TrafficLightColor.GREEN);
+
+        for(int i = 0; i < 9; i++){
+            System.out.println(t1.getColor());
+            t1.waitForChange();
+        }
+
+        t1.cancel();
+    }
+}
