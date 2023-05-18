162e3eb6a7fff97af55e5d361ded41e37711e4f1
marsabatini <marco_rsabatini@hotmail.com>
2023-05-18 16:12:23 -0300

    V. 1.07

---
+++ Cap_13/Exemplo0005/SomeTest.java
@@ -0,0 +1,10 @@
+package Cap_13.Exemplo0005;
+
+    // Interface funcional genérica com dois parâmetros
+    // que retorna um resultado boolean.
+
+public interface SomeTest<T> {
+
+    boolean test(T n, T m);
+    
+}
