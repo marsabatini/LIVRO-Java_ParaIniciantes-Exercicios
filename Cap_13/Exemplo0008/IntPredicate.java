f55dfbf3e0b31459720ee924bd19a9e7e9abadf6
marsabatini <marco_rsabatini@hotmail.com>
2023-05-19 14:41:48 -0300

    V. 1.08

---
+++ Cap_13/Exemplo0008/IntPredicate.java
@@ -0,0 +1,10 @@
+package Cap_13.Exemplo0008;
+
+    // Interface funcional para predicados numéricos
+    // que opera com valores inteiros.
+
+public interface IntPredicate {
+
+    boolean test(int n);
+    
+}
