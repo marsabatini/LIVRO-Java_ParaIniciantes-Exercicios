50a47354ab67dc860aa5bf4f2cd5ae0fa9a5fd95
marsabatini <marco_rsabatini@hotmail.com>
2023-05-20 06:51:11 -0300

    V. 1.09

---
+++ Cap_13/Exemplo0010/MyIntNumPredicate.java
@@ -0,0 +1,10 @@
+package Cap_13.Exemplo0010;
+
+// Interface funcional para predicados numéricos
+// para referenciar qualquer instância.
+
+public interface MyIntNumPredicate {
+
+    boolean test(MyIntNum mv, int n);
+
+}
