7e0bb83e2deb89d7c5916c8bad0d2a62d21b151a
marsabatini <marco_rsabatini@hotmail.com>
2023-05-21 09:06:10 -0300

    V. 2.00

---
+++ Cap_14/Exemplo0003/AppletSkel.java
@@ -0,0 +1,43 @@
+package Cap_14.Exemplo0003;
+
+// Esboço de applet baseado em AWT.
+
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="AppletSkel" width=300 height=100>
+</applet>
+*/
+
+@Deprecated
+public class AppletSkel extends Applet{
+
+    // Primeiro chamado.
+    public void init(){
+        // Inicialização
+    }
+
+    // Segundo a ser chamado após init().
+    // Também é chamado sempre que o applet é reiniciado.
+    public void start(){
+        // inicia ou retoma a execução
+    }
+
+    // Chamado quando o apllet é interrompido.
+    public void stop(){
+        // suspende a execução
+    }
+
+    // Chamado quando o applet é encerrado.
+    // Este é o último método executado.
+    public void destroy(){
+        // executa atividades de encerramento
+    }
+
+    // Chamado quando a janela de applet basedo em AWT
+    // deve ser restaurada.
+    public void paint(Ghaphics g){
+        // volta a exibir o conteúdo da janela
+    }
+}
