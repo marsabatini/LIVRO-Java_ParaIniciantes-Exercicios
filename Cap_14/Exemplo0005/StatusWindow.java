9164c2348adc89edfaaef4e6bce3c5129f234194
marsabatini <marco_rsabatini@hotmail.com>
2023-05-21 09:10:21 -0300

    V. 2.00

---
+++ Cap_14/Exemplo0005/StatusWindow.java
@@ -0,0 +1,31 @@
+package Cap_14.Exemplo0005;
+
+// Usando a janela de status
+
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="StatusWindow" width=300 height=50>
+</applet>
+*/
+
+@Deprecated
+public class StatusWindow extends Applet {
+
+    // Exibe a mensagem na janela do applet
+    public void paint(Ghaphics g){
+
+        g.drawString("This is in the applet window.", 10, 20);
+        g.showStatus("This is show in the status window.");
+    }
+}
+
+
+
+
+
+
+
+
+
