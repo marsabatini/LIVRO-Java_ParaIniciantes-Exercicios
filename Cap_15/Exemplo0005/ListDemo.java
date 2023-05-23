faca64108684a352c4b3e8c331448270262052ea
marsabatini <marco_rsabatini@hotmail.com>
2023-05-23 14:43:48 -0300

    V. 2.02

---
+++ Cap_15/Exemplo0005/ListDemo.java
@@ -0,0 +1,64 @@
+package Cap_15.Exemplo0005;
+
+    // Demonstra uma JList simples
+
+import Cap_12.Exemplo0005.D;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ListDemo implements ListSelectionListener{
+
+    JList<String> jlst;
+    JLabel jlab;
+    JScrollPane jscrlp;
+
+    // Cria um array de nomes
+    String names[] = { "Sherry", "Jon", "Rachel",
+                    "Sasha", "Josselyn", "Randy",
+                    "Tom", "Mary", "Ken",
+                    "Andrew", "Matt", "Todd" };
+
+    ListDemo(){
+
+        // Cria um contêiner JFrame
+        JFrame jfrm = new JFrame("List Demo");
+
+        // Especifica um layout de fluxo.
+        jfrm.setLayout(new FlowLayout());
+
+        // Fornece um tamanho inicial para o quadro.
+        jfrm.setSize(200, 160);
+
+        // Encerra o programa quando o usuário fechar o aplicativo.
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Cria uma JList
+        jlst = new JList<String>(names);
+
+        // Define o modo de selećão da lista como selećão simples
+        jlst.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        // Adiciona a lista a uma painel de rolagem
+        jscrlp = new JScrollPane(jlst);
+
+        // Define um tamanho para o painel de rolagem
+        jscrlp.setPreferredSize(new Dimension(120, 90));
+
+        // Cria um rótulo para exibir a selećão
+        jlab = new JLabel("Please, choose a name.");
+
+        // Adiciona o tratador da selećão
+        jlst.addListSelectionListener(this);
+
+        // Adiciona a lista e o rótulo ao painel de conteúdo
+        jfrm.add(jscrlp);
+        jfrm.add(jlab);
+
+        // Exibe o quadro.
+        jfrm.setVisible(true);
+    }
+
+}
