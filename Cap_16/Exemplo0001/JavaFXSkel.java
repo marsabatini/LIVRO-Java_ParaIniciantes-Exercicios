5dc42cca83790bd74d0e28066c08dd3f96b17df6
marsabatini <marco_rsabatini@hotmail.com>
2023-05-24 16:18:24 -0300

    V. 2.03

---
+++ Cap_16/Exemplo0001/JavaFXSkel.java
@@ -0,0 +1,24 @@
+package Cap_16.Exemplo0001;
+
+    // ESboco de um aplicativo JavaFX
+
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+
+public class JavaFXSkel extends Application{
+
+    public static void main(String[] args) {
+
+        System.out.println("Lauching JavaFX application");
+
+        // Inicializa o aplicativo JavaFx chamado launch()
+        launch(args);
+    }
+
+    // Sobrepõe o método start()
+    public void start(Stage myStage){
+        
+    }
+}
