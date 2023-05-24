58db6324c936b8e2d771699f5c80fe80eee73574
marsabatini <marco_rsabatini@hotmail.com>
2023-05-24 19:42:34 -0300

    V. 2.03

--- Cap_16/Exemplo0004/CheckBoxDemo.java
+++ Cap_16/Exemplo0004/CheckBoxDemo.java
@@ -3,12 +3,15 @@ package Cap_16.Exemplo0004;
     // Demonstra caixas de selećão
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.stage.*;
-import javafx.scene.layout.*;
-import javafx.scene.control.*;
-import javafx.event.*;
-import javafx.geometry.*;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
 public class CheckBoxDemo extends Application {
 
@@ -60,12 +63,83 @@ public class CheckBoxDemo extends Application {
         Label selected = new Label("");
 
         // Cria as caixas de selećão
+        cbSmartphone = new CheckBox("Smartphne");
+        cbTablet = new CheckBox("Tablet");
+        cbNotebook = new CheckBox("Notebook");
+        cbDesktop = new CheckBox("Desktop");
 
+        // Trata eventos de aćão das caixas de selećão
+        cbSmartphone.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(cbSmartphone.isSelected()){
+                    response.setText("Smartphone was just selected.");
+                } else {
+                    response.setText("Smartphone was just cleared.");
+                }
+
+                showAll();
+            }
+        });
+
+        cbTablet.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(cbTablet.isSelected()){
+                    response.setText("Tablet was just selected");
+                } else {
+                    response.setText("Tablet was just cleared.");
+                }
+
+                showAll();
+            }
+        });
+
+        cbNotebook.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(cbNotebook.isSelected()){
+                    response.setText("Notebook was just selected");
+                } else {
+                    response.setText("Notebook was just cleared.");
+                }
+
+                showAll();
+            }
+        });
+
+        cbDesktop.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(cbDesktop.isSelected()){
+                    response.setText("Desktop was just selected");
+                } else {
+                    response.setText("Desktop was just cleared.");
+                }
+
+                showAll();
+            }
+        });
 
         // Adiciona o rótulo ao grafo de cena
-        rootNode.getChildren().addAll(btnUp, btnDown, response);
+        rootNode.getChildren().addAll(heading, cbSmartphone, cbTablet, cbNotebook, cbDesktop, response, selected);
 
         // Exibe o palco
         myStage.show();
+
+        showAll();
+    }
+
+    // Atualiza e exibe as selećões
+    void showAll(){
+        computers = " ";
+        if(cbSmartphone.isSelected())
+            computers += "Smartphone ";
+        if(cbTablet.isSelected())
+            computers += "Tablet ";
+        if(cbNotebook.isSelected())
+            computers += "Notebook ";
+        if(cbDesktop.isSelected())
+            computers += "Desktop ";
     }
 }
