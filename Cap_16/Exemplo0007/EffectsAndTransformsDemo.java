7314ff9f4e1afd690ddd2ae3ff6a206b5b648956
marsabatini <marco_rsabatini@hotmail.com>
2023-05-25 15:01:46 -0300

    V. 2.04

--- Cap_16/Exemplo0007/EffectsAndTransformsDemo.java
+++ Cap_16/Exemplo0007/EffectsAndTransformsDemo.java
@@ -3,6 +3,8 @@ package Cap_16.Exemplo0007;
     // Demonstra a rotacao, o redimensionamento, a reflaxao e o desfoque.
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -58,6 +60,52 @@ public class EffectsAndTransformsDemo extends Application {
         reflection.setBottomOpacity(0.3);
         reflect.setEffect(reflection);
 
-        
+        // Trata os eventos de aćão do botão Rotate
+        btnRotate.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // Sempre que o botão é pressionado, ele é girado 30 graus
+                // ao redor de seu centro.
+                angle += 15.0;
+
+                rotate.setAngle(angle);
+                rotate.setPivotX(btnRotate.getWidth() / 2);
+                rotate.setPivotY(btnRotate.getHeight() / 2);
+            }
+        });
+
+        btnScale.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                scaleFactor += 0.1;
+                if(scaleFactor > 2.0)
+                    scaleFactor = 0.4;
+
+                scale.setX(scaleFactor);
+                scale.setY(scaleFactor);
+            }
+        });
+
+        btnBlur.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // Sempre que o botão é pressionado, seu status de desfoque muda.
+                if(blurVal == 10.0){
+                    blurVal = 1.0;
+                    btnBlur.setEffect(null);
+                    btnBlur.setText("Blur off");
+                } else {
+                    blurVal++;
+                    btnBlur.setEffect(blur);
+                    btnBlur.setText("Blur on");
+                }
+                blur.setWidth(blurVal);
+                blur.setHeight(blurVal);
+            }
+        });
+
+        rootNode.getChildren().addAll(btnRotate, btnScale, btnBlur, reflect);
+
+        myStage.show();
     }
 }
