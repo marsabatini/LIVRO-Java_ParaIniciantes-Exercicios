766277cddcf1d2bad5209c86060cdd7d04821133
marsabatini <marco_rsabatini@hotmail.com>
2023-05-25 14:30:17 -0300

    V. 2.04

--- Cap_16/Exemplo0006/TextFieldDemo.java
+++ Cap_16/Exemplo0006/TextFieldDemo.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -38,6 +39,28 @@ public class TextFieldDemo extends Application {
         tf = new TextField();
 
         // Define solicitaćão
-        
+        tf.setPromptText("Enter a name.");
+
+        // Define o número de colunas desejado.
+        tf.setPrefColumnCount(15);
+
+        /*
+            Usa uma expressão lambda para tratar eventos de aćão do campo de
+            texto. Eventos de aćão são gerados quando ENTER é pressionado
+            enquanto o campo de text está com foco de recebimento
+            de entrada. Nesse caso, o texto do campo é obtido e exibido.
+         */
+        tf.setOnAction( (ae) -> response.setText("Enter pressed. Name is: " +  tf.getText()));
+
+        // Usa uma expressão lambda para obter o texto do campo quando o botão
+        // é pressionado.
+        btnGetText.setOnAction( (ae) -> response.setText("Button pressed. Name is: " + tf.getText()));
+
+        Separator separator = new Separator();
+        separator.setPrefWidth(180);
+
+        rootNode.getChildren().addAll(tf, btnGetText, separator, response);
+
+        myStage.show();
     }
 }
